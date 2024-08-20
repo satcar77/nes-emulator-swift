38f6cd0b4624c33b74f6056d054605b54b1fc5f8
satcar77 <satkardhakal@gmail.com>
2024-08-20 14:39:51 -0500

    Initial Commit

---
+++ nesemu/nesemuApp.swift
@@ -0,0 +1,17 @@
+//
+//  nesemuApp.swift
+//  nesemu
+//
+//  Created by Dhakal, Satkar on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct nesemuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
